642918abcbec8ba17bb2e20aa2cf84b1eaa81d9a
Doobyeol <doobyeol@gmail.com>
2021-12-17 17:49:32 +0900

    Update #4 - Main
    구현 클래스를 인터페이스로 변경
    #4 는 여기서 마무리.
    스프링으로 넘어감!

--- Spring_Boot/JAVA_OPP/src/main/java/com/fastcampus/javaopp/Main.java
+++ Spring_Boot/JAVA_OPP/src/main/java/com/fastcampus/javaopp/Main.java
@@ -1,12 +1,15 @@
 package com.fastcampus.javaopp;
 
 import com.fastcampus.javaopp.logic.BubbleSort;
+import com.fastcampus.javaopp.logic.JavaSort;
+import com.fastcampus.javaopp.logic.Sort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
+        //BubbleSort<String> sort = new BubbleSort<>();
+        Sort<String> sort = new JavaSort<>();
         System.out.println("(result)" + sort.sort(Arrays.asList(args)));
     }
 }
