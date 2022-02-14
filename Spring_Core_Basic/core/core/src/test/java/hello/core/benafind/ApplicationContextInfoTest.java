cbef55ff4e31fc12b459f1e8bc77d11b367c4015
doobyeol <doobyeol@gmail.com>
2022-02-14 22:08:28 +0900

    Spring Core 컨테이너에 등록된 모든 빈 조회

---
+++ Spring_Core_Basic/core/core/src/test/java/hello/core/benafind/ApplicationContextInfoTest.java
@@ -0,0 +1,43 @@
+package hello.core.benafind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames){
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + " object = " + bean);
+        }
+    }
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames(); // 스프링에 등록된 모든 빈 이름을 조회
+        for (String beanDefinitionName : beanDefinitionNames){
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            // 사용자가 직접 등록한 애플리케이션 Bean
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                Object bean = ac.getBean(beanDefinitionName); // 빈 이름으로 빈 객체를 조회한다.
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
+            
+            // 스프링이 내부에서 사용하는 Bean
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_INFRASTRUCTURE){
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
+        }
+    }
+}
