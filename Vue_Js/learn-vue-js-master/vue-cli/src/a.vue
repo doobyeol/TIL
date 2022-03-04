93077f2ba6f6d5bfacd2fa1fe2088b9b0b72d26f
doobyeol <doobyeol@gmail.com>
2022-03-04 17:23:47 +0900

    vue 기초 cVue CLI

---
+++ Vue_Js/learn-vue-js-master/vue-cli/src/a.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- HTML -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+  // Javascript - 인스턴스 옵션
+
+  // var appHeader = {
+  //     template : '<div>header</div>',
+  //     methods:{
+  //         addNum: function(){
+  //         }
+  //     }
+  // }
+  methods: {
+    addNum: function () {},
+  },
+};
+</script>
+
+<style>
+/* CSS */
+</style>
